42ca83d871bf6f7f7ebe5ac0440779cf323330c7
tonysnickers <antoine.mendy@hotmail.fr>
2022-12-04 23:38:46 +0100

    input value sended to setTodo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TypeInterface from '../model/typeInterface'
 import Form from './Form'
 
 const TodoList = () => {
+  const [todos, setTodos] = useState<TypeInterface[]>([])
+
   return (
     <div>
-      <Form/>
+      <Form setTodos={setTodos} todos={todos}/>
       <h1>todo list</h1>
     </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
 import TodoList from "../components/TodoList";
-import TypeInterface from "../model/typeInterface";
 
 const Home = () => {
-  const [todos, setTodos] = useState<TypeInterface[]>([])
   return (
     <div style={{display: "flex", justifyContent: "center", marginTop: "90px"}}>
       <TodoList />
